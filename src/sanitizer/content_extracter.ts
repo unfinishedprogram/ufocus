3e370a1f885780dc16e4f561b7c5731cdfd4b42a
Noah Labrecque <noah.labrecque@gmail.com>
2024-01-20 11:45:31 -0500

    skeliton

---
+++ src/sanitizer/content_extracter.ts
@@ -0,0 +1,6 @@
+import { SanitizerInput } from "./types";
+
+// Extracts important content from raw dom handle
+export default function extractContent(doc: Document): SanitizerInput {
+
+}
--- src/sanitizer/types.ts
+++ src/sanitizer/types.ts
@@ -1,3 +1,9 @@
-export type SanitizationInput = {
-    content: string,
+export type SanitizerInput = {
+    url: string,
+    headers: {
+        type: "h1" | "h2" | "h3" | "h4" | "h5" | "h6",
+        content: string,
+    }[],
+    main_content: string,
+    nav?: string
 }
